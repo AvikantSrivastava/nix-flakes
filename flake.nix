9dcdb3f0d93a80ec6fef2316a28c36273f529e58
Avikant Srivastava <asdavikant@gmail.com>
2025-09-01 00:36:17 +0200

    update root flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
-  description = "Top-level flake aggregating sub-flakes";
+  description = "Top-level flake";
 
-  inputs.go.url = "https://avikant.com/nix-flakes/go";
-  inputs.python.url = "https://avikant.com/nix-flakes/python";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, go, python }: 
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.python = python.devShells.${system};
-      devShells.go     = go.devShells.${system};
-    });
+  # Use local path inputs for sub-flakes
+  inputs.go = { url = "path:./go"; };
+  inputs.python = { url = "path:./python"; };
+
+  outputs = { self, nixpkgs, flake-utils, go, python }:
+    flake-utils.lib.eachDefaultSystem (system:
+      {
+        devShells.python = python.devShells.${system};
+        devShells.go     = go.devShells.${system};
+      });
 }
 
